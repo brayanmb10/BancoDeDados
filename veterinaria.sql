198d2349e6bff996093fe054c0b50b6bb39dbeb5
brayan <brayanmendesbarros@gmail.com>
2026-07-30 12:24:03 -0300

    Add files via upload

---
+++ veterinaria.sql
@@ -0,0 +1,194 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 30/07/2026 às 17:23
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `veterinaria`
+--
+CREATE DATABASE IF NOT EXISTS `veterinaria` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `veterinaria`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `animal`
+--
+
+CREATE TABLE `animal` (
+  `idani` int(11) NOT NULL,
+  `idcliente` int(11) DEFAULT NULL,
+  `especie` varchar(255) NOT NULL,
+  `raca` varchar(100) NOT NULL,
+  `datanascimento` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `animal`
+--
+
+INSERT INTO `animal` (`idani`, `idcliente`, `especie`, `raca`, `datanascimento`) VALUES
+(1, 1, 'cachorro', 'pincher', '2008-09-06'),
+(2, 2, 'gato', 'INDEFINIDA', '2020-03-03'),
+(3, 3, 'tartaruga', 'INDEFINIDA', '2010-02-18');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `atendimento`
+--
+
+CREATE TABLE `atendimento` (
+  `idatendimento` int(11) NOT NULL,
+  `idcliente` int(11) DEFAULT NULL,
+  `idani` int(11) DEFAULT NULL,
+  `especie` varchar(255) NOT NULL,
+  `raca` varchar(100) NOT NULL,
+  `dataatendimento` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `idcliente` int(11) NOT NULL,
+  `nomecli` varchar(255) NOT NULL,
+  `telefone` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `cpf` varchar(100) NOT NULL,
+  `endereco` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `cliente`
+--
+
+INSERT INTO `cliente` (`idcliente`, `nomecli`, `telefone`, `email`, `cpf`, `endereco`) VALUES
+(1, 'brayan mendes', '930350200', 'brayanmaua@gmail.com', '12312312309', 'rua belterra 291'),
+(2, 'pedro mazza', '967679898', 'pedro@gmail.com', '12312312306', 'rua belterra 298'),
+(3, 'gustavo fontes', '965467809', 'GFontes@gmail.com', '12345678909', 'rua belterra 299');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `veterinario`
+--
+
+CREATE TABLE `veterinario` (
+  `idvet` int(11) NOT NULL,
+  `nomevet` varchar(255) NOT NULL,
+  `telefone` varchar(255) NOT NULL,
+  `especialidade` varchar(100) NOT NULL,
+  `crmv` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `veterinario`
+--
+
+INSERT INTO `veterinario` (`idvet`, `nomevet`, `telefone`, `especialidade`, `crmv`) VALUES
+(1, 'jhennifer', '930350700', 'domesticos', 'a7b2c9d1-8e3f-4a6b-9c12-4f5e6d7c8b9a'),
+(2, 'eduardo', '930367670', 'silvestre', '3f2504e0-4f89-4b23-a555-7348e55e69e2'),
+(3, 'humberto', '950367679', 'cirurgia', '8c4d2e1f-6a5b-4c3d-8e1f-2a3b4c5d6e7f');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `animal`
+--
+ALTER TABLE `animal`
+  ADD PRIMARY KEY (`idani`),
+  ADD KEY `idcliente` (`idcliente`);
+
+--
+-- Índices de tabela `atendimento`
+--
+ALTER TABLE `atendimento`
+  ADD PRIMARY KEY (`idatendimento`),
+  ADD KEY `idcliente` (`idcliente`),
+  ADD KEY `idani` (`idani`);
+
+--
+-- Índices de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`idcliente`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `cpf` (`cpf`);
+
+--
+-- Índices de tabela `veterinario`
+--
+ALTER TABLE `veterinario`
+  ADD PRIMARY KEY (`idvet`),
+  ADD UNIQUE KEY `especialidade` (`especialidade`),
+  ADD UNIQUE KEY `crmv` (`crmv`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `animal`
+--
+ALTER TABLE `animal`
+  MODIFY `idani` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de tabela `atendimento`
+--
+ALTER TABLE `atendimento`
+  MODIFY `idatendimento` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `idcliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de tabela `veterinario`
+--
+ALTER TABLE `veterinario`
+  MODIFY `idvet` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `animal`
+--
+ALTER TABLE `animal`
+  ADD CONSTRAINT `animal_ibfk_1` FOREIGN KEY (`idcliente`) REFERENCES `cliente` (`idcliente`);
+
+--
+-- Restrições para tabelas `atendimento`
+--
+ALTER TABLE `atendimento`
+  ADD CONSTRAINT `atendimento_ibfk_1` FOREIGN KEY (`idcliente`) REFERENCES `cliente` (`idcliente`),
+  ADD CONSTRAINT `atendimento_ibfk_2` FOREIGN KEY (`idani`) REFERENCES `animal` (`idani`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
